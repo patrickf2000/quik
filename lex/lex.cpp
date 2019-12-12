e326dcda3e6a50ce59bdac9f73ed036781832e0a
patrick <dev@patrickflynn.co>
2019-12-12 17:54:16 -0500

    added support for include and ID tokens; confirmed test program is working

--- lex/lex.cpp
+++ lex/lex.cpp
@@ -2,7 +2,28 @@
 
 //Tokenize a string
 std::vector<Token> tokenize(std::string line) {
+	line += " ";	//No purpose, makes my life easier :)
+	
 	std::vector<Token> tokens;
+	std::string current = "";
+	
+	for (auto c : line) {
+		if (c == ' ') {
+			Token t;
+			
+			if (current == "include") {
+				t.type = TokenType::INCLUDE;
+			} else {
+				t.type = TokenType::ID;
+				t.id = current.c_str();
+			}
+			
+			tokens.push_back(t);
+			current = "";
+		} else {
+			current += c;
+		}
+	}
 	
 	return tokens;
 }
