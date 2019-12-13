5a548b9e2f037e589ea7f70a3030203aa24b9b2f
patrick <dev@patrickflynn.co>
2019-12-13 12:54:02 -0500

    added shell mode to parser test

--- parser/test/parser_test.cpp
+++ parser/test/parser_test.cpp
@@ -6,10 +6,29 @@
 #include <ast.hh>
 
 #include "build.hh"
+#include "parse.hh"
 
 int main(int argc, char *argv[]) {
-	auto lines = load_source(argv[1]);
-	AstNode *top = build_ast(lines);
-	print_tree(top);
+	if (argc == 1) {
+		Line l;
+		l.no = 1;
+		
+		std::string ln = "";
+		std::cout << "$> ";
+		std::getline(std::cin, ln);
+		
+		l.original = ln;
+		l.tokens = tokenize(ln);
+		
+		AstNode *node = build_node(l);
+		
+		std::cout << std::endl << "AST: " << std::endl;
+		print_tree(node);
+	} else {
+		auto lines = load_source(argv[1]);
+		AstNode *top = build_ast(lines);
+		print_tree(top);
+	}
+	
 	return 0;
 }
