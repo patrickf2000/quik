46b153e59faef3c6a86e862a18aca1fed93f96f9
patrick <dev@patrickflynn.co>
2020-01-02 09:25:27 -0500

    fixed slight error causing test to fail

--- compiler/x86/x86_array.cpp
+++ compiler/x86/x86_array.cpp
@@ -12,6 +12,7 @@ void Asm_x86::build_arr_dec(AstNode *node) {
 	v.stack_pos = stack_pos;
 	v.size = ard->children.size();
 	v.is_array = true;
+	v.is_param = false;
 	vars[ard->get_name()] = v;
 	
 	//Determine the stack position and type
