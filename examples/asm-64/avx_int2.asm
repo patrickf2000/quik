c7277fa125bc66d9a242bf51d0d0a26a9292a984
patrick <dev@patrickflynn.co>
2020-01-20 20:17:45 -0500

    updated examples

--- examples/asm-64/avx_int2.asm
+++ examples/asm-64/avx_int2.asm
@@ -1,4 +1,13 @@
 section .data
+	flt_1 dd 1.100000
+	flt_2 dd 2.200000
+	flt_3 dd 3.300000
+	flt_4 dd 4.400000
+	flt_5 dd 5.500000
+	flt_6 dd 6.600000
+	flt_7 dd 7.700000
+	flt_8 dd 8.800000
+	STR_1 db "%f",0xA,0
 
 section .bss
 
@@ -9,9 +18,73 @@ section .text
 	extern fflush
 	extern input_int
 	extern print_int
+	global add_float
+	global add_int
 	global main
 
-main:
+add_float:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 128
+	
+	movss xmm0, [flt_1]
+	movss [rbp-36], xmm0
+	movss xmm0, [flt_2]
+	movss [rbp-32], xmm0
+	movss xmm0, [flt_3]
+	movss [rbp-28], xmm0
+	movss xmm0, [flt_4]
+	movss [rbp-24], xmm0
+	movss xmm0, [flt_5]
+	movss [rbp-20], xmm0
+	movss xmm0, [flt_6]
+	movss [rbp-16], xmm0
+	movss xmm0, [flt_7]
+	movss [rbp-12], xmm0
+	movss xmm0, [flt_8]
+	movss [rbp-8], xmm0
+	
+	vmovupd ymm0, [rbp-36]
+	vmovupd ymm1, [rbp-36]
+	vhaddps ymm0, ymm0, ymm1
+	
+	vextractf128 xmm4, ymm0, 0
+	vextractf128 xmm5, ymm0, 1
+	
+	extractps [rbp-52], xmm4, 0
+	extractps [rbp-48], xmm4, 1
+	extractps [rbp-44], xmm5, 0
+	extractps [rbp-40], xmm5, 1
+	
+	
+	mov dword [rbp-56], 0
+	
+	jmp L2
+L1:
+	mov eax, [rbp-56]
+	cdqe
+	mov rbx, rax
+	mov rax, [rbp-52]
+	imul rbx, 4
+	movss xmm0, [rbp-52+rbx]
+	movss [rbp-60], xmm0
+	
+	mov edi, STR_1
+	movss xmm0, [rbp-60]
+	cvtss2sd xmm0, xmm0
+	call printf
+	
+	add dword [rbp-56], 1
+	
+L2:
+	mov eax, [rbp-56]
+	cmp eax, 4
+	jl L1
+	
+	leave
+	ret
+	
+add_int:
 	push rbp
 	mov rbp, rsp
 	sub rsp, 196
@@ -46,8 +119,8 @@ main:
 	
 	mov dword [rbp-56], 0
 	
-	jmp L2
-L1:
+	jmp L4
+L3:
 	mov eax, [rbp-56]
 	cdqe
 	mov rbx, rax
@@ -62,10 +135,22 @@ L1:
 	
 	add dword [rbp-56], 1
 	
-L2:
+L4:
 	mov eax, [rbp-56]
 	cmp eax, 4
-	jl L1
+	jl L3
+	
+	leave
+	ret
+	
+main:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 48
+	
+	call add_float
+	
+	call add_int
 	
 	leave
 	ret
