e83070c10ca5db3fac83c4af029a1e4fba0f1cc4
patrick <dev@patrickflynn.co>
2020-02-05 15:33:55 -0500

    added multiple parameter passing support (ARM)

--- compiler/armv7/armv7_funcs.cpp
+++ compiler/armv7/armv7_funcs.cpp
@@ -14,19 +14,25 @@ void Asm_Armv7::build_func_dec(AstNode *node) {
 //Builds a function call
 void Asm_Armv7::build_func_call(AstNode *node) {
 	AstFuncCall *fc = static_cast<AstFuncCall *>(node);
+	int r_index = 0;
 	
 	for (auto child : fc->children) {
+		std::string reg = "r" + std::to_string(r_index);
+		++r_index;
+	
 		switch (child->type) {
 			//Integers
 			case AstType::Int: {
 				AstInt *i = static_cast<AstInt *>(child);
-				sec_text.push_back("mov r0, #" + std::to_string(i->get_val()));
+				std::string ln = "mov " + reg + ", #";
+				ln += std::to_string(i->get_val());
+				sec_text.push_back(ln);
 			} break;
 		
 			//Strings
 			case AstType::Str: {
 				auto name = build_string(child);
-				sec_text.push_back("ldr r0, ." + name);
+				sec_text.push_back("ldr " + reg + ", ." + name);
 			} break;
 			
 			//Other variables
@@ -34,7 +40,8 @@ void Asm_Armv7::build_func_call(AstNode *node) {
 				AstID *id = static_cast<AstID *>(child);
 				Var v = vars[id->get_name()];
 				
-				std::string ln = "ldr r0, [fp, #-";
+				std::string ln = "ldr " + reg;
+				ln += ", [fp, #-";
 				ln += std::to_string(v.stack_pos) + "]";
 				sec_text.push_back(ln);
 			} break;
