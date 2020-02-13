8a3fd5c602fff0f415b81d6f3261f1d61e1bb2ab
patrick <dev@patrickflynn.co>
2020-02-12 21:02:53 -0500

    moved function and variable code into separate files (ltac)

---
+++ ltac/ltac_funcs.cpp
@@ -0,0 +1,24 @@
+#include "ltac_build.hh"
+
+//Builds an extern function declaration
+void LTAC_Builder::build_extern(AstNode *node) {
+	AstAttrNode *fd = static_cast<AstAttrNode *>(node);
+	
+	LtacNode *lbl = new LtacNode(LTAC::Extern);
+	lbl->val = fd->get_name();
+	
+	file->children.push_back(lbl);
+}
+
+//Builds a function declaration
+void LTAC_Builder::build_func_dec(AstNode *node) {
+	AstFuncDec *fd = static_cast<AstFuncDec *>(node);
+	
+	LtacNode *lbl = new LtacNode(LTAC::Label);
+	lbl->val = fd->get_name();
+	
+	LtacNode *setup = new LtacNode(LTAC::Setup);
+	
+	file->children.push_back(lbl);
+	file->children.push_back(setup);
+}
