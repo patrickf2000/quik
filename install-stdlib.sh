02890955e04b2c746b9714d9758d761cdd7a0d47
patrickf2000 <dev@patrickflynn.co>
2020-03-20 08:42:08 -0400

    made slight changes to standard library build scripts

--- install-stdlib.sh
+++ install-stdlib.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+INSTALL_PATH="/usr/local/include/quik"
+
+if [ ! -d $INSTALL_PATH ] ; then
+	sudo mkdir -p $INSTALL_PATH
+fi
+
 if [ ! -f ./build/libqkstdlib.so ] ; then
+	sudo cp -v ./stdlib/headers/*.qk $INSTALL_PATH
 	echo "Error: The standard library does not seem to be built."
 	echo "Plese build it, and try again."
 	exit 1
@@ -9,13 +16,5 @@ fi
 sudo cp -v ./build/libqkstdlib.so /usr/local/lib
 sudo cp -v ./build/libqkstdlib32.so /usr/local/lib
 sudo ldconfig
-
-INSTALL_PATH="/usr/local/include/quik"
-
-if [ ! -d $INSTALL_PATH ] ; then
-	sudo mkdir -p $INSTALL_PATH
-fi
-
-sudo cp -v ./stdlib/headers/*.qk $INSTALL_PATH
 
 echo "Done!"
